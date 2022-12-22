1472f35abaa1539e3d9fd4a631c7e03b26a0f8ef
Eliking500 <oshogweoghie12@gmail.com>
2022-12-22 13:24:50 -0800

    main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+char *_strcat(char *dest, char *src);
+char *_strncat(char *det, char *sr, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *a1, char *a2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
