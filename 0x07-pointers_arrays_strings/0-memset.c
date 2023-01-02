df11d340b3652a7fa70305c81a1e943dc7ecb303
Eliking500 <oshogweoghie12@gmail.com>
2023-01-02 11:15:36 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - file memory with a constant byte.
+ * _memset - fills memory with a constant byte.
  * @s: source string
  * @b: the constant byte for filling
  * @n: length of buffer
@@ -9,6 +9,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
+
 	while (i < n)
 	{
 		*(s + i) = b;
