a8c5851cf1496e97c3d83c9fb3503d0203662e1c
Eliking500 <oshogweoghie12@gmail.com>
2023-01-02 01:55:51 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,13 @@
 #include"main.h"
+
 /**
  * print_square - print a square using the character #
- * @size: is the ize of the square
+ *
+ * @size: is the size of the square
+ *
  * Return: Always 0 (Success)
  */
+
 void print_square(int size)
 {
 	int row, column;
