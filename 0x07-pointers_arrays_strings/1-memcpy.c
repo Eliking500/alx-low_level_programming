27301e4acbabf51a1ecdfccd2ce5307b68771f2f
Eliking500 <oshogweoghie12@gmail.com>
2023-01-07 16:58:06 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
  * _memcpy - function that copies memory area
  *
  * @dest: buffer where we will copy to
- * @src: what weare to copy
+ * @src: what we are to copy
  * @n: n bytes of @src
  *
  * Return: Always 0 (Success)
