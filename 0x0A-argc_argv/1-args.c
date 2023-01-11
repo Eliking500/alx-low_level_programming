5efdcaf26ea53e1c89f50c76a0f74297356dc8eb
Eliking500 <oshogweoghie12@gmail.com>
2023-01-11 15:29:52 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of argument
+ * passed into it
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the argument passed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	int i = 0, m;
+
+	while (i < argc)
+	{
+		m = 1;
+		i++;
+	}
+	printf("%d\n", m);
+	return (0);
+}
